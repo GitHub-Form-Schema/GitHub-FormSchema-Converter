16de9333fd567f23e9ee99a057e9d07624e8dc73
ghdtpdnjs <hsw1805@kookmin.ac.kr>
2023-10-02 17:57:52 +0900

    dropdown + textarea 추가

--- dist/github-formschema-converter.js
+++ dist/github-formschema-converter.js
@@ -1,5 +1,40 @@
 const yaml = require('js-yaml');
 
+
+
+const textarea2HTML = (attributes) => {
+    let {label, description, placeholder, value, render} = attributes;
+    return `
+<h3>${label}</h3> 
+<p>${description}</p>
+<p><textarea cols="50" rows="10" placeholder='${placeholder}' style="background-color:#f6f8fa">${value}</textarea></p>
+`
+}
+
+
+const dropdown2HTML = (attributes) => {
+    console.log(attributes)
+    const {label, description,options}= attributes;
+    return `
+    <h3>${label}</h3>
+    <p>${description}</p>
+    <select>
+    ${getList(options)}    
+    </select>
+    `
+}
+
+
+const getList = (options) => {
+    let optionCommand="";
+    
+    for(let i = 0; i < options.length; i++){
+        optionCommand+=`<option>${options[i]}</options>`;
+    }
+
+    return optionCommand;
+}
+
 const yaml2html = (yamlStr) => {
     const loadedYamls = yaml.load(yamlStr);
     
@@ -8,18 +43,14 @@ const yaml2html = (yamlStr) => {
         if (loadedYaml.type === "textarea") {
             res.push(textarea2HTML(loadedYaml.attributes))
         }
+        else if(loadedYaml.type ==="dropdown"){
+            res.push(dropdown2HTML(loadedYaml.attributes))
+        }
     });
 
     return res
 }
 
-const textarea2HTML = (attributes) => {
-    let {label, description, placeholder, value, render} = attributes;
-    return `
-<h3>${label}</h3> 
-<p>${description}</p>
-<p><textarea cols="50" rows="10" placeholder='${placeholder}'>${value}</textarea></p>
-`
-}
+
 
 exports.yaml2html = yaml2html;
--- example/test1.js
+++ example/test1.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { yaml2html } = require('../dist/github-formschema-converter');
 
-console.log(yaml2html(fs.readFileSync('example/yaml/test1.yaml')));
+console.log(yaml2html(fs.readFileSync(__dirname + '\\yaml\\test1.yaml')));
 
-console.log(yaml2html(fs.readFileSync('example/yaml/test1.yaml'))[0]);
+console.log(yaml2html(fs.readFileSync(__dirname + '\\yaml\\test1.yaml'))[0]);
